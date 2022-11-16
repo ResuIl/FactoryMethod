60f6768bccf5524cf643ebc7c12a64513813a3f8
Rəsul <71267982+ResuIl@users.noreply.github.com>
2022-11-16 08:37:43 +0400

    Add project files.

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,50 @@
+﻿public interface IButton
+{
+    void Render();
+    void OnClick();
+}
+
+public class WindowsButton : IButton
+{
+    public void OnClick() => Console.WriteLine("Windows button Click");
+    public void Render() => Console.WriteLine("Windows button Render");
+}
+
+public class WebButton : IButton
+{
+    public void OnClick() => Console.WriteLine("Web button Click");
+    public void Render() => Console.WriteLine("Web render Render");
+}
+
+public abstract class Dialog
+{
+    public void Render()
+    {
+        IButton Button = CreateButton();
+        Button.OnClick();
+        Button.Render();
+    }
+
+    public abstract IButton CreateButton();
+}
+
+public class WindowDialog : Dialog
+{
+    public override IButton CreateButton() => new WindowsButton();
+}
+
+public class HtmlDialog : Dialog
+{
+    public override IButton CreateButton() => new WebButton();
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Dialog wd = new WindowDialog();
+        wd.Render();
+        wd = new HtmlDialog();
+        wd.Render();
+    }
+}
